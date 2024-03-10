89aae6b3e5cd48b76ee225bd2f98c20f7c6d2628
João Pedro <jpedropsss@gmail.com>
2024-03-10 12:30:18 -0400

    atualização

--- Conceitos-C-Cpp/EX1/Ex1.c
+++ Conceitos-C-Cpp/EX1/Ex1.c
@@ -1,16 +1,9 @@
-/*Faça um programa que, contendo uma estrutura com capacidade para até 100 elementos do tipo inteiro,  
-solicite ao usuário a quantidade desejada de elementos a serem armazenados (um número inferior a  100) e, em seguida,
-colete os dados para cada uma das posições. Ao final, o programa imprimirá todos  os valores fornecidos pelo usuário.
-Construir e utilizar a implementação de procedimentos/funções  para realizar as operações de inserção e impressão dos nós da lista. */
-
 #include <stdio.h>
-
 
 struct Lista
 {
     int elementos[100];
     int quantidade;
-    
 };
 
 void entradadedados(struct Lista *lista) {
@@ -18,25 +11,18 @@ void entradadedados(struct Lista *lista) {
     printf("Digite a quantidade de elementos para armazenar na lista:\n");
     scanf("%d", &lista->quantidade);
 
-    
-
-    for ( i = 1; i <= lista->quantidade; i++)
+    for (i = 1; i <= lista->quantidade; i++)
     {
-
-        printf("Digite o elemtento %d\n",i);
+        printf("Digite o elemento %d:\n", i);
         scanf("%d", &lista->elementos[i]);
-    
     }
-    
-
-
 }
 
-void saidadedados(struct Lista *lista ){
+void saidadedados(struct Lista *lista) {
     int i;
-     for ( i = 1; i <= lista->quantidade; i++){
-    printf("O valor do elemento %d : %d \n ", i, lista->elementos[i]);
-}
+    for (i = 1; i <= lista->quantidade; i++) {
+        printf("O valor do elemento %d: %d\n", i, lista->elementos[i]);
+    }
 }
 
 int main() {
@@ -45,5 +31,3 @@ int main() {
     saidadedados(&minhaLista);
     return 0;
 }
-
-
---
+++ ListaEx/Lista1/Q1.c
@@ -0,0 +1,7 @@
+/* Elabore um programa que leia a leitura de 10 valores inteiros, armazene-os em um vetor de 10 posições.  
+Cada valor lido deve ocupar uma posição única na estrutura.
+ Em seguida, percorra o vetor para  contabilizar o quantitativo de números pares e quantidade de números ímpares.
+Ao final, imprima os  resultados. 
+*/
+
+#include<stdio.h>;
